a9724497fe27dd268138c30fba9f71b2fb13859c
Neel Patel <neelpatel@smu.edu>
2025-02-19 13:32:10 -0600

    init react frontend

---
+++ frontend/src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import {APIProvider} from '@vis.gl/react-google-maps';
+
+const App = () => (
+    <APIProvider apiKey={'Your API key here'} onLoad={() => console.log('Maps API has loaded.')}>
+        <h1>Hello, world!</h1>
+    </APIProvider>
+);
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
+
+export default App;
