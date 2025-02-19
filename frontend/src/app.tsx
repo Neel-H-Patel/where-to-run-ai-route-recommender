0bde37b9033729566403ba9849dd554f188b07b9
Neel Patel <neelpatel@smu.edu>
2025-02-19 13:43:20 -0600

    add google map to it

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import {APIProvider} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
 
 const App = () => (
-    <APIProvider apiKey={'Your API key here'} onLoad={() => console.log('Maps API has loaded.')}>
+    <APIProvider apiKey={'API key goes here'} onLoad={() => console.log('Maps API has loaded.')}>
         <h1>Hello, world!</h1>
+        <Map
+            defaultZoom={13}
+            defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+            onCameraChanged={ (ev: MapCameraChangedEvent) =>
+                console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+            }>
+        </Map>
     </APIProvider>
 );
 
