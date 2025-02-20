bc93ec74b75d5a397d1c1ac5449b33ac8372f481
Neel Patel <neelpatel@smu.edu>
2025-02-20 10:41:09 -0600

    update type to match Strava return type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,10 +2,10 @@ export interface Route {
     id: number;
     name: string;
     distance: number; // in meters
-    elevation_gain: number; // in meters
+    elev_difference: number; // in meters
     avg_grade: number; // percentage
     climb_category: number;
     start_latlng: [number, number]; // Latitude, Longitude
     end_latlng: [number, number]; // Latitude, Longitude
-    polyline: string; // Encoded polyline for map rendering
+    points: string; // Encoded polyline for map rendering
 }
