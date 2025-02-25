aacacf288a0edc90f164c4c381011373336cf049
Neel Patel <neelpatel@smu.edu>
2025-02-25 15:49:30 -0600

    UI changes to RouteMap

--- new-frontend/src/components/RouteMap.tsx
+++ new-frontend/src/components/RouteMap.tsx
@@ -11,22 +11,24 @@ interface RouteMapProps {
 
 const RouteMap: React.FC<RouteMapProps> = ({ selectedRoute, center }) => {
     return (
-        <Map defaultZoom={13} center={center} className="w-full h-[600px] rounded-md shadow">
-            {selectedRoute && (
-                <>
-                    <Marker
-                        key={`start-${selectedRoute.id}`}
-                        position={{ lat: selectedRoute.start_latlng[0], lng: selectedRoute.start_latlng[1] }}
-                        title={`${selectedRoute.name} - Start`}
-                    />
-                    <Marker
-                        key={`end-${selectedRoute.id}`}
-                        position={{ lat: selectedRoute.end_latlng[0], lng: selectedRoute.end_latlng[1] }}
-                        title={`${selectedRoute.name} - End`}
-                    />
-                </>
-            )}
-        </Map>
+        <div className="relative w-full md:w-1/2">
+            <Map defaultZoom={13} center={center} className="w-full h-[500px] md:h-[600px] rounded-lg shadow-lg border dark:border-gray-700">
+                {selectedRoute && (
+                    <>
+                        <Marker
+                            key={`start-${selectedRoute.id}`}
+                            position={{ lat: selectedRoute.start_latlng[0], lng: selectedRoute.start_latlng[1] }}
+                            title={`${selectedRoute.name} - Start`}
+                        />
+                        <Marker
+                            key={`end-${selectedRoute.id}`}
+                            position={{ lat: selectedRoute.end_latlng[0], lng: selectedRoute.end_latlng[1] }}
+                            title={`${selectedRoute.name} - End`}
+                        />
+                    </>
+                )}
+            </Map>
+        </div>
     );
 };
 
