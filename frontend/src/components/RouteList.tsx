8dc6a24e3d41acc501526d024c26903c5d72f289
Neel Patel <neelpatel@smu.edu>
2025-02-25 00:12:17 -0600

    add files to new frontend

--- frontend/src/components/RouteList.tsx
+++ frontend/src/components/RouteList.tsx
@@ -4,9 +4,11 @@ import { Route } from "../types";
 interface RouteListProps {
     routes: Route[];
     loading: boolean;
+    selectedRoute: Route | null;
+    setSelectedRoute: (route: Route | null) => void;  // 🔹 Allow selection
 }
 
-const RouteList: React.FC<RouteListProps> = ({ routes, loading }) => {
+const RouteList: React.FC<RouteListProps> = ({ routes, loading, selectedRoute, setSelectedRoute }) => {
     return (
         <div>
             <h2>Route Recommendations</h2>
@@ -15,11 +17,21 @@ const RouteList: React.FC<RouteListProps> = ({ routes, loading }) => {
             {!loading && routes.length > 0 && (
                 <ul>
                     {routes.map((route) => (
-                        <li key={route.id}>
-                            <b>{route.name}</b> - {(route.distance / 1000).toFixed(1)} km, {route.elevation_gain} m elevation gain
-                            <br />Avg Grade: {route.avg_grade}%, Climb Category: {route.climb_category}
-                            <br />Start: {route.start_latlng[0]}, {route.start_latlng[1]}
-                            <br />End: {route.end_latlng[0]}, {route.end_latlng[1]}
+                        <li
+                            key={route.id}
+                            onClick={() => setSelectedRoute(route)}
+                            style={{
+                                cursor: "pointer",
+                                padding: "8px",
+                                borderBottom: "1px solid #ccc",
+                                backgroundColor: selectedRoute?.id === route.id ? "#e0f7fa" : "transparent"  // Highlight selected
+                            }}
+                        >
+                            <b>{route.name}</b> - {(route.distance / 1000).toFixed(1)} km, {route.elevation_gain} m
+                            elevation gain
+                            <br/>Avg Grade: {route.avg_grade}%, Climb Category: {route.climb_category}
+                            <br/>Start: {route.start_latlng[0]}, {route.start_latlng[1]}
+                            <br/>End: {route.end_latlng[0]}, {route.end_latlng[1]}
                         </li>
                     ))}
                 </ul>
--- frontend/src/components/RouteMap.tsx
+++ frontend/src/components/RouteMap.tsx
@@ -5,21 +5,21 @@ import { Route } from "../types";
 const DEFAULT_CENTER = { lat: 40.73061, lng: -73.935242 }; // Default location (NYC)
 
 interface RouteMapProps {
-    routes: Route[];
+    selectedRoute: Route | null;
 }
 
-const RouteMap: React.FC<RouteMapProps> = ({ routes }) => {
+const RouteMap: React.FC<RouteMapProps> = ({ selectedRoute }) => {
     return (
         <Map defaultZoom={13} defaultCenter={DEFAULT_CENTER} style={{ width: "100%", height: "500px" }}>
-            {routes.map((route) => (
-                <React.Fragment key={route.id}>
+            {selectedRoute && (
+                <>
                     {/* Start Marker */}
-                    <Marker position={{ lat: route.start_latlng[0], lng: route.start_latlng[1] }} title={route.name} />
+                    <Marker position={{ lat: selectedRoute.start_latlng[0], lng: selectedRoute.start_latlng[1] }} title={selectedRoute.name} />
 
                     {/* End Marker */}
-                    <Marker position={{ lat: route.end_latlng[0], lng: route.end_latlng[1] }} title={`${route.name} (End)`} />
-                </React.Fragment>
-            ))}
+                    <Marker position={{ lat: selectedRoute.end_latlng[0], lng: selectedRoute.end_latlng[1] }} title={`${selectedRoute.name} (End)`} />
+                </>
+            )}
         </Map>
     );
 };
